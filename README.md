efd7b5de2ba11666d5ab25b133aad7851bfce834
Juan José Torres <juanjtorres@pm.me>
2020-05-03 18:00:52 -0500

    comando evolution_chain y actualización del readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Pokémon
+
+Esta aplicación de Django consiste principalmente en un comando que puede ser invocado por consola para obtener la
+información de una cadena de evolución según su ID y en un servicio web que retorna la información de un pokémon según
+su nombre.
+
+Para correr la aplicación después de clonar el repositorio debes instalar sus dependencias con 
+`pip install -r requirements.txt`
+
+Para usar el comando de la consola usa el siguiente código:
+`python manage.py evolution_chain <ID>`
+
+Para obtener información de un pokemon usa la siguiente dirección: `/name/<nombre>`
---
+++ search_pokemon/management/commands/evolution_chain.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand, CommandError
+from ...services import logic
+import pprint
+
+
+class Command(BaseCommand):
+    help = 'Encuentra e imprime la información de los Pokémones de una cadena de evolución según su ID'
+
+    def add_arguments(self, parser):
+        parser.add_argument('id', type=int, help='ID de la cadena de evolución pokémon a buscar')
+
+    def handle(self, *args, **kwargs):
+        try:
+            self.stdout.write("La información de la cadena de evolución con id %s es" % kwargs['id'])
+            pprint.pprint(logic.evolution_chain(kwargs['id']))
+        except:
+            raise CommandError('Hubo un error')
