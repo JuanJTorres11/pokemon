bd65afcbc097b30d1a65012cfc9626d7bd20cc64
Juan José Torres <juanjtorres@pm.me>
2020-05-03 19:40:32 -0500

    models

--- search_pokemon/admin.py
+++ search_pokemon/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Pokemon)
+admin.site.register(PokemonEvolution)
+admin.site.register(PokemonStat)
+admin.site.register(Stat)
+admin.site.register(EvolutionChain)
--- search_pokemon/models.py
+++ search_pokemon/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Stat (models.Model):
+    STATS = [
+        ("speed", "speed"),
+        ("special-defense", "special-defense"),
+        ("special-attack", "special-attack"),
+        ("defense", "defense"),
+        ("attack", "attack"),
+        ("hp", "hp")
+    ]
+    name = models.CharField(max_length=15, choices=STATS, unique=True)
+
+
+class Pokemon (models.Model):
+    name = models.CharField(max_length=30, primary_key=True)
+    id = models.PositiveIntegerField(unique=True)
+    height = models.PositiveIntegerField()
+    weight = models.PositiveIntegerField()
+    stats = models.ManyToManyField(Stat, through='PokemonStat')
+
+
+class PokemonStat(models.Model):
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    stat = models.ForeignKey(Stat, on_delete=models.CASCADE)
+    base = models.PositiveIntegerField()
+
+
+class EvolutionChain(models.Model):
+    id = models.PositiveIntegerField(primary_key=True)
+    pokemon = models.ManyToManyField(Pokemon, through="PokemonEvolution")
+
+
+class PokemonEvolution(models.Model):
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    evolution_chain = models.ForeignKey(EvolutionChain, on_delete=models.CASCADE)
+    position = models.PositiveSmallIntegerField()
